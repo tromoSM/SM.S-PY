00dae5aa4d05649aef735890e0ded8303ef02f6c
tromoSM <tromosm7@gmail.com>
2025-09-30 23:08:54 +0530

    GUI/Emmet-AutoCSS-SM.css.js

---
+++ GUI/Emmet-AutoCSS-SM.css.js
@@ -0,0 +1,23 @@
+document.querySelectorAll(`tromoSM-head-main`).forEach(fuh=>{
+    fuh.insertAdjacentHTML(`afterend`,"<br>")
+    fuh.style.fontWeight="700"
+    fuh.style.fontSize="28px"
+    fuh.style.textAlign="center"
+    fuh.style.lineHeight="2"
+})
+document.querySelectorAll(`tr-norm`).forEach(fuh=>{
+    fuh.insertAdjacentHTML(`afterend`,"<br>")
+    fuh.style.fontWeight="400"
+    fuh.style.fontSize="16px"
+    fuh.style.textAlign="justify"
+})
+function Func_display_temp_privacy(){
+ document.querySelector(`[tromoSM=rr-temp-notifcation-warr]`).style.animation="far 1s 0ms forwards ease 1"
+}
+document.querySelectorAll(`tromoSM-ui-close`).forEach(fuh=>{
+   fuh.innerHTML="×"
+   const $uiR = fuh.getAttribute(`ui-r`);
+   fuh.setAttribute("onclick" , `Func_display_temp_privacy()`) 
+   //just a use like a if else thing with a custom attribute when u got like more bcloise buttons for tos of smth
+   //i didnt do the relative ahh path function trhing
+})
