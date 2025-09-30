189bb2fe9dfceea38dd8ccbf08d7660e62a86b32
tromoSM <tromosm7@gmail.com>
2025-10-01 01:12:35 +0530

    GUI/letSniff.js

---
+++ GUI/begginLet-Sniff.js
@@ -0,0 +1,14 @@
+/*
+TO YO DUMBASS IN DA FUTURE 
+DO NOT REMOVE BC,
+  ERROR 67ux
+  TOS FUNC
+  TOS CLOSE FUNC
+  PRIVACY FUNC
+  PRIVACY CLOSE FUNC
+  IF (textahh==null)
+   */
+let $nullrnot=0;
+let $ui_RU;
+let $ahh = "geniusAHH-67UXPY";
+let $freeloader = "geniusAHH-67UXPY";
