80d3cd491f7f680764d8c9901bef05eeae17e2e7
tromoSM <tromosm7@gmail.com>
2025-10-05 23:45:06 +0530

    Assets/PrivacyStatement.req.js
    Assets/PrivacyStatement.req.js : add

---
+++ Assets/PrivacyStatement.req.js
@@ -0,0 +1,1 @@
+const $getUI4oprivacyStatementUse = "We do not collect any data from our users. None of your data is anywhere other than your device. Network requests : https://ifconfig.me to get your ip address." 
