ec1d3bc09a28262b6c73bfb220d9ee83f86c8f4e
tromoSM <tromosm7@gmail.com>
2025-10-08 00:51:06 +0530

    GUI/begginLet-Sniff.js

--- GUI/begginLet-Sniff.js
+++ GUI/begginLet-Sniff.js
@@ -4,11 +4,13 @@ DO NOT REMOVE BC,
   ERROR 67ux
   TOS FUNC
   TOS CLOSE FUNC
+  TOS VERSION
   PRIVACY FUNC
   PRIVACY CLOSE FUNC
   IF (textahh==null)
    */
 let $nullrnot=0;
 let $ui_RU;
+let $tos_version;
 let $ahh = "geniusAHH-67UXPY";
 let $freeloader = "geniusAHH-67UXPY";
