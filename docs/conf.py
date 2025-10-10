6ff42a9b5370f49f5226d54f593d34f23232563d
tromoSM <tromosm7@gmail.com>
2025-10-11 05:18:35 +0530

    docs/conf.py
    :add theme

--- docs/conf.py
+++ docs/conf.py
@@ -2,3 +2,6 @@ project = 'SM.S-PY'
 author = 'tromoSM'   
 master_doc = 'index'
 release = '1.0.0'     
+
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
