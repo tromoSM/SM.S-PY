6eb383a6ecc3a5c09fff09a3d214118c21207f64
tromoSM <tromosm7@gmail.com>
2025-10-12 01:32:44 +0530

    GUI/textahh.js
    --upgrade :V.08si
    - fix :err old :inf

--- GUI/textahh.js
+++ GUI/textahh.js
@@ -29,4 +29,4 @@ const $defaultahhs=String.raw`C:\Users\TromoSM`.replace(/\\/g, '/');
 const $recent=[
     'file 1','file 2','file 3','file 4','file 5','file 6','file 7','file 8','file 9','file 10','file 11','file 12','file 13','file 14','file 15','file 16','file 17','file 18','file 19','file 20','file 21','file 22','file 23','file 24','file 25','file 26', 'file 27', 'file 28', 'file 29','file 30','file 31','file 32','file 33','file 34','file 35','file 36','file 37','file 38','file 39','file 40','file 41',
 ]
-$$version=`V0.7 beta --PRIVATE Release (10.01.2025 as MM.DD.YYYY)`
+$$version=`V0.8si betaSi--PUBLIC Release (10.12.2025 as MM.DD.YYYY)`
