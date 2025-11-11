9638f7b1d7bd7f5d00c797c78d920a38f87ba848
tromoSM <tromosm7@gmail.com>
2025-11-12 03:40:59 +0530

    GUI/loader.js

---
+++ GUI/loader.js
@@ -0,0 +1,13 @@
+//not in use of the current version
+function loadAll(gurt_yo){
+    let reaf=document.querySelector('[data-dynamic]')
+    if(reaf)reaf.remove()
+    const gurt$$eat$$my$$ass=document.createElement('script')
+    gurt$$eat$$my$$ass.src=gurt_yo
+    gurt$$eat$$my$$ass.dataset.dynamic=`true`
+    gurt$$eat$$my$$ass.onload=()=>{
+     document.head.appendChild(gurt$$eat$$my$$ass)
+    
+    }
+//not finished--
+}
